e277289067b2d115ea8d06e67d495398f0d61cc3
Rhishabh Hattarki <rhish9h@gmail.com>
2024-04-11 14:02:48 -0700

    test folder setup

---
+++ EmployeeManagementAPI/EmployeeManagementAPITests/Repositories/EmployeeRepositoryTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace EmployeeManagementAPITests.Repositories
+{
+	public class EmployeeRepositoryTests
+	{
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
+
