d7e42919ba3bc52ab87d1bbe6206fc09bd4ad847
Rhishabh Hattarki <rhish9h@gmail.com>
2024-04-11 14:58:55 -0700

    get employee by id test

--- EmployeeManagementAPI/EmployeeManagementAPITests/Repositories/EmployeeRepositoryTests.cs
+++ EmployeeManagementAPI/EmployeeManagementAPITests/Repositories/EmployeeRepositoryTests.cs
@@ -2,8 +2,6 @@
 using EmployeeManagementAPI.Models;
 using EmployeeManagementAPI.Repositories;
 using Microsoft.EntityFrameworkCore;
-using Moq;
-using NuGet.Protocol.Core.Types;
 
 namespace EmployeeManagementAPITests.Repositories
 {
@@ -19,6 +17,14 @@ namespace EmployeeManagementAPITests.Repositories
             _options = new DbContextOptionsBuilder<EmployeeContext>()
                 .UseInMemoryDatabase(databaseName: "TestDatabase")
                 .Options;
+
+            // Clear database
+            using (var context = new EmployeeContext(_options))
+            {
+                // Clear the Employees table
+                context.Employees.RemoveRange(context.Employees);
+                context.SaveChanges();
+            }
 
             // Seed the in-memory database with test data
             using (var context = new EmployeeContext(_options))
@@ -54,6 +60,25 @@ namespace EmployeeManagementAPITests.Repositories
             // Assert: Compare the result with expected employees
             Assert.That(result, Is.Not.Null); // Ensure result is not null
             Assert.That(result.Count(), Is.EqualTo(expectedEmployees.Count)); // Ensure the correct number of employees is returned
+        }
+
+        [Test]
+        public async Task GetEmployeeByIdAsync_ReturnsCorrectEmployee()
+        {
+            // Arrange
+            int idToFind = 1;
+            var expectedEmployee = new Employee { Id = idToFind, Name = "John Doe", Email = "john@example.com", DateOfBirth = new DateTime(1990, 1, 1), Department = "IT" };
+
+            // Act
+            var result = await _repository.GetEmployeeByIdAsync(idToFind);
+
+            // Assert
+            Assert.That(result, Is.Not.Null); // Ensure result is not null
+            Assert.That(result.Id, Is.EqualTo(expectedEmployee.Id)); // Ensure correct employee is returned
+            Assert.That(result.Name, Is.EqualTo(expectedEmployee.Name));
+            Assert.That(result.Email, Is.EqualTo(expectedEmployee.Email));
+            Assert.That(result.DateOfBirth, Is.EqualTo(expectedEmployee.DateOfBirth));
+            Assert.That(result.Department, Is.EqualTo(expectedEmployee.Department));
         }
     }
 }
